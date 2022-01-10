4a356f2dd773027d15cd68996fcb1be75bf94033
dionysus11 <52480056+dionysus11-source@users.noreply.github.com>
2022-01-10 19:57:22 +0900

    [Fix] Change class name

--- CronManager.py
+++ CronManager.py
@@ -1,7 +1,8 @@
 import json
-import torrent_see
+import TorrentDownloader 
 
-downloader = torrent_see.TorrentDownloader("산다")
+downloader = TorrentDownloader.TorrentDownloader()
+downloader.keyword = '산다'
 downloader.download_torrent_file()
 
 with open('schedule.json','r') as f:
