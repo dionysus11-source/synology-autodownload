4a04815bb73f601556351b596a7cd23e5f6bbc44
dionysus11 <52480056+dionysus11-source@users.noreply.github.com>
2022-01-14 11:46:36 +0900

    version update and remove sec_options

--- image/run_test.py
+++ image/run_test.py
@@ -2,11 +2,7 @@ import TorrentDownloader
 print('start test')
 
 def test_download_success():
-    downloader = TorrentDownloader.TorrentDownloader('옥탑방')
+    downloader = TorrentDownloader.TorrentDownloader('어서와')
     result  = downloader.download_torrent_file()
     assert result == True, "success"
    
-def test_download_fail():
-    downloader = TorrentDownloader.TorrentDownloader('테스트')
-    result  = downloader.download_torrent_file()
-    assert result == False, "success"
